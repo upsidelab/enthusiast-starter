32b5dc2c9500ca42db5ae069cbe233bc32300e9a
Rafal Cymerys <rafal@upsidelab.io>
2025-07-18 11:59:25 +0200

    feat: enable pdf agent and plugin by default

--- config/settings_override.py
+++ config/settings_override.py
@@ -3,5 +3,15 @@
 
 CATALOG_PRODUCT_SOURCE_PLUGINS = {
     "Sample Product Source": "enthusiast_source_sample.SampleProductSource",
-    "My Plugin": "enthusiast_custom.MyPlugin"
+}
+
+CATALOG_DOCUMENT_SOURCE_PLUGINS = {
+    "Sample Document Source": "enthusiast_source_sample.SampleDocumentSource",
+    "Fetch PDFs": "enthusiast_custom.examples.pdf_documents_plugin.PDFDocumentSourcePlugin",
+}
+
+AVAILABLE_AGENTS = {
+    "Product Search Agent": "agent.core.agents.product_search_react_agent",
+    "Question Answer Agent": "agent.core.agents.tool_calling_agent",
+    "PDF Query Agent": "enthusiast_custom.examples.pdf_agent",
 }
