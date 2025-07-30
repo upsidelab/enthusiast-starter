6ae53963d2f437ff34e55c678845e79785c88e68
Rafal Cymerys <rafal@upsidelab.io>
2025-07-30 17:11:36 +0200

    feat: update config structure

--- src/enthusiast_custom/examples/pdf_agent/config.py
+++ src/enthusiast_custom/examples/pdf_agent/config.py
@@ -1,5 +1,5 @@
-from enthusiast_common.config import AgentConfigWithDefaults, LLMConfig, LLMToolConfig
-from langchain_core.prompts import ChatPromptTemplate
+from enthusiast_common.config import AgentConfigWithDefaults, LLMToolConfig
+from enthusiast_common.config.base import ChatPromptTemplateConfig
 
 from .tools.pdf_context_tool import ContextSearchTool
 from .agent import ExamplePDFAgent
@@ -9,11 +9,11 @@ from .prompt import PDF_AGENT_SYSTEM_PROMPT
 def get_config(conversation_id: int, streaming: bool) -> AgentConfigWithDefaults:
     return AgentConfigWithDefaults(
         conversation_id=conversation_id,
-        prompt_template=ChatPromptTemplate.from_messages(
-            [
+        prompt_template=ChatPromptTemplateConfig(
+            messages=[
                 (
                     "system",
-                    PDF_AGENT_SYSTEM_PROMPT,
+                    PDF_AGENT_SYSTEM_PROMPT
                 ),
                 ("placeholder", "{chat_history}"),
                 ("human", "{input}"),
