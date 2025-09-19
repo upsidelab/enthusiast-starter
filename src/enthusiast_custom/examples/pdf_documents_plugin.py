6e33db5995202569d1ee6dbf35b8865875967690
Damian Sowiński <dsowinski2@gmail.com>
2025-09-19 11:58:45 +0200

    chore: make plugin config input as separate field for one file

--- src/enthusiast_custom/examples/pdf_documents_plugin.py
+++ src/enthusiast_custom/examples/pdf_documents_plugin.py
@@ -5,33 +5,34 @@ import requests
 from enthusiast_common import DocumentSourcePlugin, DocumentDetails
 from enthusiast_common.utils import RequiredFieldsModel
 from langchain_community.document_loaders import PyPDFLoader
-from pydantic import Field, Json
+from pydantic import Field
 
 logger = logging.getLogger(__name__)
 
 
 class PDFSourceConfig(RequiredFieldsModel):
-    data: Json = Field(title="PDF urls", description="Json containing values where key is document name and value is url.", default='{"filename.pdf": "https://example.com"}')
+    url: str = Field(title="URL", description="URL to PDF file.")
+    filename: str = Field(title="Filename", description="PDF filename.")
 
 class PDFDocumentSourcePlugin(DocumentSourcePlugin):
     CONFIGURATION_ARGS = PDFSourceConfig
 
     def fetch(self) -> list[DocumentDetails]:
         results = []
-        data = self.CONFIGURATION_ARGS.data
-        for title, url in data.items():
-            try:
-                response = requests.get(url)
-                response.raise_for_status()
+        title = self.CONFIGURATION_ARGS.filename
+        url = self.CONFIGURATION_ARGS.url
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
 
-                temp_path = Path(f"/tmp/temp.pdf")
-                with open(temp_path, "wb") as f:
-                    f.write(response.content)
+            temp_path = Path(f"/tmp/temp.pdf")
+            with open(temp_path, "wb") as f:
+                f.write(response.content)
 
-                loader = PyPDFLoader(str(temp_path))
-                for index, page in enumerate(loader.lazy_load()):
-                    results.append(DocumentDetails(url=f"{url}/{index}", title=title, content=page.page_content))
-                return results
+            loader = PyPDFLoader(str(temp_path))
+            for index, page in enumerate(loader.lazy_load()):
+                results.append(DocumentDetails(url=f"{url}/{index}", title=title, content=page.page_content))
+            return results
 
-            except Exception as e:
-                logger.error(f"Failed to load {url} ({title}): {e}")
+        except Exception as e:
+            logger.error(f"Failed to load {url} ({title}): {e}")
