6735d1d6fed3d38003fb4c9823ac7872f35740af
Dawid Mularczyk <dawid.mularczyk@onet.pl>
2026-06-03 12:15:40 +0200

    chore: update ExampleDocumentContextAgent import path

--- config/settings_override.py
+++ config/settings_override.py
@@ -24,7 +24,7 @@ AVAILABLE_AGENTS = [
     'enthusiast_agent_product_search.ProductSearchAgent',
     'enthusiast_agent_catalog_web_import.CatalogWebImportAgent',
     'enthusiast_agent_invoice_scanning.InvoiceScanningAgent',
-    'enthusiast_custom.examples.document_context_agent.ExampleDocumentContextAgent'
+    'enthusiast_custom.ExampleDocumentContextAgent'
 ]
 
 AVAILABLE_AGENTIC_EXECUTION_DEFINITIONS = [
